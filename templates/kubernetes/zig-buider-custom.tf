d6c9af43cc14ed72c3aa5f82641d79cbfbe700ad
Mark Brown <mark.brown8790@gmail.com>
2024-07-27 22:29:58 +0000

    Updating zig docker container to add missing tool

--- templates/kubernetes/zig-buider-custom.tf
+++ templates/kubernetes/zig-buider-custom.tf
@@ -83,7 +83,7 @@ resource "coder_agent" "main" {
     set -e
 
     # Update Zig to latest master version
-    zig update
+    # zig update
 
     # install and start code-server
     curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
