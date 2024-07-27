efa429bd0db9d6a58432f7921c1c31d7b0c78589
Mark Brown <mark.brown8790@gmail.com>
2024-07-27 17:09:47 +0000

    Updated configurations and included firefox for audio testing

--- dockerfiles/ubuntu-mate/startup.sh
+++ dockerfiles/ubuntu-mate/startup.sh
@@ -4,6 +4,7 @@ if [ ! -f $HOME/.vnc/passwd ] ; then
     if  [ -z "$PASSWORD" ] ; then
         PASSWORD=`pwgen -c -n -1 12`
         echo -e "PASSWORD = $PASSWORD" > $HOME/password.txt
+        echo "Generated password: " && cat $HOME/password.txt
     fi
 
     echo "$USER:$PASSWORD" | chpasswd
@@ -25,15 +26,5 @@ else
         rm -rf /tmp/.X1*
     fi
 fi
-
-# if [ ! -z "$NGROK" ] ; then
-#         case "$NGROK" in
-#             [yY]|[yY][eE][sS])
-#                 su ubuntu -c "$HOME/ngrok/ngrok http 6080 --log $HOME/ngrok/ngrok.log --log-format json" &
-#                 sleep 5
-#                 NGROK_URL=`curl -s http://127.0.0.1:4040/status | grep -P "http://.*?ngrok.io" -oh`
-#                 su ubuntu -c "echo -e 'Ngrok URL = $NGROK_URL/vnc.html' > $HOME/ngrok/Ngrok_URL.txt"
-#         esac
-# fi
 
 /usr/bin/supervisord -n
