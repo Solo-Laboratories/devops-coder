ce73bece4c23506990f8686a78c4ac5edb17cd67
Mark Brown <mark.brown8790@gmail.com>
2024-07-27 22:14:54 +0000

    Updated template to update zig on startup

--- templates/kubernetes/zig-buider-custom.tf
+++ templates/kubernetes/zig-buider-custom.tf
@@ -82,6 +82,9 @@ resource "coder_agent" "main" {
   startup_script = <<-EOT
     set -e
 
+    # Update Zig to latest master version
+    zig update
+
     # install and start code-server
     curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
     /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
