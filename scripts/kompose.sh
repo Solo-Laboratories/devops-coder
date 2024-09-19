76abad07f5e644dc361c9339258fb9e1af33e7b7
Mark Brown <mark.brown8790@gmail.com>
2024-09-19 02:41:10 +0000

    Changed kompose file to copy executable; updates script to pass in kompose version

--- scripts/kompose.sh
+++ scripts/kompose.sh
@@ -1,11 +1,13 @@
 set -e
 
+# $1 is kompose version; for example "1.34.0"
+
 # Install Kompose if not present
 if [ ! -f $PWD/kompose ]; then
   echo "Downloading Kompose..."
-  curl -L https://github.com/kubernetes/kompose/releases/download/v1.34.0/kompose-linux-amd64 -o kompose
+  curl -L https://github.com/kubernetes/kompose/releases/download/v${1}/kompose-linux-amd64 -o kompose
 fi
 
 echo "Moving Kompose to /usr/local/bin..."
 chmod +x $PWD/kompose
-sudo mv $PWD/kompose /usr/local/bin/kompose
+sudo cp $PWD/kompose /usr/local/bin/kompose
