dc0595410f8f114b4769009eda9269bda3254be9
Mark Brown <mark.brown8790@gmail.com>
2024-09-19 02:37:57 +0000

    Updating kompose script to move to /usr/local/bin rather than messing with .bashrc

--- scripts/kompose.sh
+++ scripts/kompose.sh
@@ -6,9 +6,6 @@ if [ ! -f $PWD/kompose ]; then
   curl -L https://github.com/kubernetes/kompose/releases/download/v1.34.0/kompose-linux-amd64 -o kompose
 fi
 
-#Check to see if exists .. If not append
-if ! grep -q "export PATH='$PATH:$PWD/kompose'" "$HOME/.bashrc"; then
-  echo "Updating PATH in ~/.bashrc for kompose..."
-  echo "\n# Kompose Executable Added to PATH" >> $HOME/.bashrc;
-  echo "export PATH='$PATH:$PWD/kompose'" >> $HOME/.bashrc;
-fi
+echo "Moving Kompose to /usr/local/bin..."
+chmod +x $PWD/kompose
+sudo mv $PWD/kompose /usr/local/bin/kompose
