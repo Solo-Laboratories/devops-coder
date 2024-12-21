210290413a5cdc30bd7590fb5bc38c30b8eb5481
Mark Brown <mark.brown8790@gmail.com>
2024-12-21 04:01:33 +0000

    feat(scripts): updated code-server to use passed in variable IF it is passed in otherwise use default

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -1,7 +1,15 @@
 set -e
 
 # $1 is code server version; for example "4.11.0"
+# Default value for VERSION 
+VAR="4.96.1" 
+# Check if a parameter is passed 
+if [ -z "$1" ]; then 
+    VERSION=$VAR 
+else 
+    VERSION=$1 
+fi
 
 # install and start code-server
-curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version $1
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version $VERSION
 /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
--- templates/kubernetes/one-ring-to-rule-them-all/main.tf
+++ templates/kubernetes/one-ring-to-rule-them-all/main.tf
@@ -178,7 +178,7 @@ resource "coder_agent" "main" {
     curl -fsSL https://raw.githubusercontent.com/Solo-Laboratories/devops-coder/main/scripts/config-setup.sh | sh -
 
     # Executes Coder server startup
-    curl -fsSL https://raw.githubusercontent.com/Solo-Laboratories/devops-coder/main/scripts/code-server.sh | sh -s -- 4.95.3
+    curl -fsSL https://raw.githubusercontent.com/Solo-Laboratories/devops-coder/main/scripts/code-server.sh | sh -
   EOT
 
   # The following metadata blocks are optional. They are used to display
