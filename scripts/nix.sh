18051ba02915ea478695ddfb6cf6c1750bd8f4dc
Mark Brown <mark.brown8790@gmail.com>
2025-06-03 15:31:18 +0000

    fix: nix installer script had non-absolute paths for during checks

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -1,13 +1,13 @@
 set -e
 
-if [ ! -f "~/.config/nix/nix.conf" ]; then
+if [ ! -f "/home/coder/.config/nix/nix.conf" ]; then
   echo "Updating nix.conf file...\n"
   mkdir -p ~/.config/nix
   echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
   echo "store = local?store=/home/coder/.nix/store&state=/home/coder/.nix/state&log=/home/coder/.nix/log" >> /home/coder/.config/nix/nix.conf
 fi
 
-if [ ! test -d "~/.nix" ]; then
+if [ ! test -d "/home/coder/.nix" ]; then
   echo "Local Nix Store is not present...\n"
   mkdir -p /home/coder/.nix/store
   mkdir -p /home/coder/.nix/state
