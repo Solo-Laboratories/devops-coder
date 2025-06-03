8eb53381f5c1bbaaaf9450c91f054d6a62ffc6b5
Mark Brown <mark.brown8790@gmail.com>
2025-06-03 15:34:55 +0000

    fix: issue in nix installer script fixed

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -1,18 +1,16 @@
 set -e
 
 if [ ! -f "/home/coder/.config/nix/nix.conf" ]; then
-  echo "Updating nix.conf file...\n"
-  mkdir -p ~/.config/nix
-  echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+  echo "Updating nix.conf file..."
+  mkdir -p /home/coder/.config/nix
+  echo "experimental-features = nix-command flakes" >> /home/coder/.config/nix/nix.conf
   echo "store = local?store=/home/coder/.nix/store&state=/home/coder/.nix/state&log=/home/coder/.nix/log" >> /home/coder/.config/nix/nix.conf
 fi
 
-if [ ! test -d "/home/coder/.nix" ]; then
-  echo "Local Nix Store is not present...\n"
-  mkdir -p /home/coder/.nix/store
-  mkdir -p /home/coder/.nix/state
-  mkdir -p /home/coder/.nix/log
+if [ ! -d "/home/coder/.nix" ]; then
+  echo "Local Nix Store is not present..."
+  mkdir -p /home/coder/.nix/store /home/coder/.nix/state /home/coder/.nix/log
 fi
 
-echo "Installing or Updating Nix...\n"
+echo "Installing or Updating Nix...
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
