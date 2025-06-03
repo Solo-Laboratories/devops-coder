d5b26b28c0f1b30947ca13536d79f7c9b9b42045
Mark Brown <mark.brown8790@gmail.com>
2025-06-03 14:11:17 +0000

    fix: issue using sh so switched to bash to see if it works

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -4,5 +4,5 @@ if command -v application_name &> /dev/null; then
   echo "Nix Package Manager Found!"
 else
   echo "Nix Package Manager not found. Installing Nix...\n"
-  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+  bash <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
 fi
