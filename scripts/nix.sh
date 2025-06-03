2f1f985b4615db2c0523da56d20b2dc7e4f74cf8
Mark Brown <mark.brown8790@gmail.com>
2025-06-03 14:08:20 +0000

    fix: updates nix.sh file to check for nix first

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -1,3 +1,8 @@
 set -e
 
-curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --no-daemon
+if command -v application_name &> /dev/null; then
+  echo "Nix Package Manager Found!"
+else
+  echo "Nix Package Manager not found. Installing Nix...\n"
+  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+fi
