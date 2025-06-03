925e8e9e1ad484901d5697d192c8dffc03d61d81
Mark Brown <mark.brown8790@gmail.com>
2025-06-03 15:28:24 +0000

    fix: updated nix bash script installer

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -4,14 +4,14 @@ if [ ! -f "~/.config/nix/nix.conf" ]; then
   echo "Updating nix.conf file...\n"
   mkdir -p ~/.config/nix
   echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
-  echo "store = local?store=~/.nix/store&state=~/.nix/state&log=~/.nix/log" >> ~/.config/nix/nix.conf
+  echo "store = local?store=/home/coder/.nix/store&state=/home/coder/.nix/state&log=/home/coder/.nix/log" >> /home/coder/.config/nix/nix.conf
 fi
 
 if [ ! test -d "~/.nix" ]; then
   echo "Local Nix Store is not present...\n"
-  mkdir -p ~/.nix/store
-  mkdir -p ~/.nix/state
-  mkdir -p ~/.nix/log
+  mkdir -p /home/coder/.nix/store
+  mkdir -p /home/coder/.nix/state
+  mkdir -p /home/coder/.nix/log
 fi
 
 echo "Installing or Updating Nix...\n"
