d9da83e885f2331fc8ea1c7b12f67ff21443754c
Mark Brown <mark.brown8790@gmail.com>
2025-06-07 15:19:36 +0000

    fix: updated nix script to check for nix installation first before installing; otherwise skip installation

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -1,4 +1,8 @@
 set -e
 
-echo "Installing or Updating Nix..."
-curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+if command -v nix &> /dev/null; then
+    echo "Nix found. To update please use 'curl -L https://nixos.org/nix/install | sh -s -- --no-daemon'"
+else
+    echo "Installing or Updating Nix..."
+    curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+fi
